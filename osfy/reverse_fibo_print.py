c2e0e98e1a53a44009036df8eea8b204c5d335bc
Siddharth Chandra <siddharthchandragzb@gmail.com>
2021-05-31 15:44:28 +0530

    Update reverse_fibo_print.py

--- osfy/reverse_fibo_print.py
+++ osfy/reverse_fibo_print.py
@@ -1,4 +1,8 @@
 
+# The task is to print fibonacci series in reverse order.
+# So, if the input n == 3, then output should be - 1 1 0
+# Similarly, for n = 6, output - 5 3 2 1 1 0
+
 def reverse_fibo_print(a, b, num):
     if (num>1):
         reverse_fibo_print(b, a+b, num-1)
