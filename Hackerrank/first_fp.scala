52f21d5d6ef4d5e83e62d6ae76c00b5c55806bc0
Siddharth Chandra <siddharthchandragzb@gmail.com>
2021-06-03 11:02:59 +0530

    Create first_fp.scala

---
+++ Hackerrank/first_fp.scala
@@ -0,0 +1,15 @@
+/*
+
+Functional Programming -> Introductions
+Link in Problems.md file
+
+Solve Me First FP
+
+*/
+
+object Solution {
+
+    def main(args: Array[String]) {
+        println(io.Source.stdin.getLines().take(2).map(_.toInt).sum)
+    }
+}
