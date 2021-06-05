648bc1343ad03950f942e8ed768bc94f79b58ad6
Siddharth Chandra <siddharthchandragzb@gmail.com>
2021-06-05 21:37:32 +0530

    Create array_n_elem_fp.scala

---
+++ Hackerrank/array_n_elem_fp.scala
@@ -0,0 +1,14 @@
+/*
+
+FP QUESTION
+LINK IN Problems.md
+
+Array Of N Elements
+
+*/
+
+import scala.io.StdIn.readInt
+    
+    def f(num:Int) : List[Int] = {
+        List.fill(num)(0) // Write your code here
+    }
