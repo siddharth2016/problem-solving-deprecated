458118e9ac7d69a2fffda92a27c32ad9c7f6604e
siddharth2016 <siddharthchandragzb@gmail.com>
2021-07-17 20:56:38 +0530

    update files

--- DSA_JP/find_missing.py
+++ DSA_JP/find_missing.py
@@ -13,3 +13,11 @@ def find(arr1, arr2):
         finder[v]-=1
 
 print(find([5, 5, 7, 7], [5, 7, 7]))
+
+def find2(arr1, arr2):
+    res = 0
+    for x in arr1+arr2:
+        res^=x
+    return res
+
+print(find2([1, 2, 3, 5,5,7,7], [1, 2, 3, 5,7,7]))
