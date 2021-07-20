8ae6d12948c86305046201522b650022c8ff577c
siddharth2016 <siddharthchandragzb@gmail.com>
2021-07-20 13:40:53 +0530

    create doubly_linked_list.py

---
+++ DSA_Course_JoseP_Udemy/linked lists/doubly_linked_list.py
@@ -0,0 +1,22 @@
+class DoublyLinkedListNode():
+    
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        
+a = DoublyLinkedListNode(1)
+b = DoublyLinkedListNode(2)
+c = DoublyLinkedListNode(3)
+
+a.next = b
+b.prev = a
+
+b.next = c
+c.prev = b
+
+temp = a
+
+while temp != None:
+    print(temp.value)
+    temp = temp.next
